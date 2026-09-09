4ef430e07c28831919685dbd1d25ed4fa33516f2
https://github.com/kudupuditejasree/coding-exercises-java <kudupuditejasree@gmail.com>
2026-09-09 16:43:18 +0530

    initial commit

---
+++ src/pt.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class pt {
+
+
+        public static void main(String[] args) {
+            System.out.println("Enter the number of rows");
+            Scanner scanner=new Scanner(System.in);
+            int num=scanner.nextInt();
+
+            for(int i=1;i<=6;i++){
+                for( i=1;i<=6;i++){
+                    System.out.println(1);
+                    //system.out.print(" ");
+                    //System.out.print(" ");
+                    //System.out.print(" "+(i+2));
+
+                }
+            }
+
+
+        }
+    }
+
